3488ab1179fee133583af8a95f9f2e7963459ac7
kzwatkins <kzw@acm.org>
2016-05-21 13:13:27 -0400

    Added way to switch to map

--- app/src/main/java/com/didactex/www/batapp2/MainActivity.java
+++ app/src/main/java/com/didactex/www/batapp2/MainActivity.java
@@ -1,13 +1,15 @@
 package com.didactex.www.batapp2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.htpBtn);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Map.class);
+                startActivity(intent);
             }
         });
     }
